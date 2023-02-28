cd7d1725f375e98abe42f6bb09fc4dd77c672be9
aleksikk <58860966+aleksikk@users.noreply.github.com>
2023-02-28 14:48:34 +0200

    -

---
+++ layouts/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+  <h5><a href="/tos">Terms & Conditions</a> <span style="font-size:6px;">•</span> <a href="http://steamcommunity.com/id/tuz1k.com/" target="_blank">Partnership</a> <span style="font-size:6px;">•</span> <a href="" target="_blank">Contact</a></h5>
+  <h6>&copy; 2017 tuz1k</h6>
+</div>
